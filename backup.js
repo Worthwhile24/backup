f2a2b7f7fb987ab333af768bcb53cc220fad5392
Worthwhile24 <125680595+Worthwhile24@users.noreply.github.com>
2023-02-17 19:00:55 +0200

    Add files via upload

---
+++ backup.js
@@ -0,0 +1,26 @@
+ require('dotenv').config();
+ const CronJob = require("cron").CronJob;
+ const Rsync = require("rsync");
+
+
+rsync = new Rsync()
+  
+ .flags("a")
+
+
+ .source(process.env.SOURCE_DIR)
+ .destination(process.env.DESTINATION_DIR);
+
+const job = new CronJob(
+    process.env.CRON_STRING, 
+    () => {
+        rsync.execute((error,code,cmd) => {
+    console.log("Backup Complete with status:" + code);
+
+});
+    },
+    null,true,
+    "America/toronto"
+);
+
+job.start();
