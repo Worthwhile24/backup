cb00eb2f1f2d16a794c36a42283dd4802f4968be
Worthwhile24 <125680595+Worthwhile24@users.noreply.github.com>
2023-02-18 16:41:22 +0200

    Update backup.js

--- backup.js
+++ backup.js
@@ -2,8 +2,15 @@ require("dotenv").config();
 const CronJob = require("cron").CronJob;
 const Rsync = require("rsync");
 
+// The value of process.platform will be:
+// Windows: win32
+// Mac: darwin
+// Ubuntu: linux
+const syncProgram = process.platform === "win32" ? "robocopy" : "rsync";
+
 // Equivalent to writing `rsync -a example-source/ example-destination/` on terminal
 rsync = new Rsync()
+  .executable(syncProgram)
   // The -a flag means "archive" to say we are copying the full directory not just a file
   .flags("a")
   // Reads from the `.env` file in the project directory
@@ -17,9 +24,19 @@ const job = new CronJob(
   process.env.CRON_STRING,
   () => {
     rsync.execute((error, code, cmd) => {
-      // List of rsync status codes
-      // https://stackoverflow.com/a/20738063
-      console.log("backup completed with status code: " + code);
+      let result;
+      if (error) {
+        // List of rsync status codes
+        // https://stackoverflow.com/a/20738063
+        result = `Code ${code} ${error?.message}`;
+      } else {
+        result = "Backup complete";
+      }
+
+      const currentDate = new Date().toISOString();
+      // Write log to the console, or will be redirected to a
+      // nohup.out file if using nohup
+      process.stdout.write(`${currentDate}: ${result}\n`);
     });
   },
   null,
